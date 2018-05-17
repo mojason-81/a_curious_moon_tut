44fa6a828007ba5e82ae3c5bd635d77dfde226e5
Jason Force <jforce@rednovalabs.com>
2018-05-17 07:57:59 -0500

    Add enceladus_events view

--- scripts/normalize.sql
+++ scripts/normalize.sql
@@ -87,3 +87,18 @@ left join requests
 left join spass_types
     on spass_types.description = import.master_plan.spass_type;
 
+drop view if exists enceladus_events;
+create view enceladus_events as
+select
+    events.id,
+    events.title,
+    events.description,
+    events.time_stamp,
+    events.time_stamp::date as date,
+    event_types.description as event
+from events
+inner join event_types
+    on event_types.id = events.event_type_id
+where target_id = (select id from targets where description = 'Enceladus')
+order by time_stamp;
+
