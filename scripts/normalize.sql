b1d9cba878d6e506c4eda9460156321d4e316e32
Jason Force <jason.r.force@gmail.com>
2018-05-17 21:45:40 -0500

    materialize enceladus_events view

--- scripts/normalize.sql
+++ scripts/normalize.sql
@@ -88,17 +88,22 @@ left join spass_types
     on spass_types.description = import.master_plan.spass_type;
 
 drop view if exists enceladus_events;
-create view enceladus_events as
+create materialized view enceladus_events as
 select
     events.id,
     events.title,
     events.description,
     events.time_stamp,
     events.time_stamp::date as date,
-    event_types.description as event
+    event_types.description as event,
+    to_tsvector(
+        concat(events.description,' ', events.title)
+    ) as search
 from events
 inner join event_types
     on event_types.id = events.event_type_id
 where target_id = (select id from targets where description = 'Enceladus')
 order by time_stamp;
 
+create index idx_event_search
+on enceladus_events using GIN(search);
